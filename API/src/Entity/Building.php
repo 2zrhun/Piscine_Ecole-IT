121c85f452688fc98918b048ce599c6bc770b691
pierre defauquet <pierredefauquet@gmail.com>
2025-10-09 13:39:06 +0200

    uploaded 3D file

--- API/src/Entity/Building.php
+++ API/src/Entity/Building.php
@@ -4,8 +4,12 @@ namespace App\Entity;
 
 use App\Repository\BuildingRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Validator\Constraints as Assert;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 #[ORM\Entity(repositoryClass: BuildingRepository::class)]
+#[Vich\Uploadable]
 class Building
 {
     #[ORM\Id]
@@ -16,8 +20,31 @@ class Building
     #[ORM\Column(length: 255)]
     private ?string $name = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, nullable: true)]
     private ?string $file = null;
+
+    #[Vich\UploadableField(mapping: 'building_images', fileNameProperty: 'file')]
+    #[Assert\File(
+    maxSize: '2G',
+    mimeTypes: [
+        'image/jpeg',
+        'image/png',
+        'image/gif',
+        'image/webp',
+        'image/jpg',
+        'video/mp4',
+        'video/quicktime',
+        'video/x-msvideo',
+        'video/mpeg',
+        'model/gltf-binary',
+        'application/octet-stream'
+    ],
+    mimeTypesMessage: 'Veuillez uploader une image, vidéo ou modèle 3D valide (GLB)'
+)]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
 
     public function getId(): ?int
     {
@@ -32,7 +59,6 @@ class Building
     public function setName(string $name): static
     {
         $this->name = $name;
-
         return $this;
     }
 
@@ -41,10 +67,34 @@ class Building
         return $this->file;
     }
 
-    public function setFile(string $file): static
+    public function setFile(?string $file): static
     {
         $this->file = $file;
+        return $this;
+    }
 
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
         return $this;
     }
 }
